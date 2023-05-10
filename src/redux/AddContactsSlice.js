fc20727db8ec47c571b0576fafecf797bf3da3c9
KhailoAndrey <hap512@gmail.com>
2023-05-11 02:01:04 +0300

    builder

--- src/redux/AddContactsSlice.js
+++ src/redux/AddContactsSlice.js
@@ -7,35 +7,36 @@ const AddContactsSlice = createSlice({
   reducers: {
     addContact: (state, action) => [...state, action.payload],
   },
-  extraReducers: {
-    [fetchContacts.pending](state) {
-      state.isLoading = true;
-    },
-    [fetchContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [fetchContacts.rejected](state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    [fetchDelContacts.fulfilled](state, action) {
-      state.items = state.items.filter(
-        contact => contact.id !== action.payload.id
-      );
-      state.error = null;
-    },
-    [fetchDelContacts.rejected](state, action) {
-      state.error = action.payload;
-    },
-    [fetchAdContacts.fulfilled](state, action) {
-      state.items.push(action.payload);
-      state.error = null;
-    },
-    [fetchAdContacts.rejected](state, action) {
-      state.error = action.payload;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(fetchDelContacts.fulfilled, (state, action) => {
+        state.items = state.items.filter(
+          contact => contact.id !== action.payload.id
+        );
+        state.error = null;
+      })
+      .addCase(fetchDelContacts.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(fetchAdContacts.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+        state.error = null;
+      })
+      .addCase(fetchAdContacts.rejected, (state, action) => {
+        state.error = action.payload;
+      });
   },
 });
 
